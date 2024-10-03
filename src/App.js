4758a97e986894e59787a2d4bc09775cb276d069
amnesia <ai28092005@gmail.com>
2024-10-03 13:15:13 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="content">
+        <p className='logo'>MK
+        <span class='logo__proffesion'>UI/UX Designer &<br/>Product Designer</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <nav className="navbar">
+          <button className="nav-button">Home</button>
+          <button className="nav-button">About</button>
+          <button className="nav-button">Work</button>
+          <button className="nav-button">News</button>
+          <button className="nav-button">Contact</button>
+        </nav>
+        <h2>Hello people! </h2>
+        <h1>We’re creative<br/>digital studio.</h1>
+        <img></img>
+      </div>
     </div>
   );
 }
