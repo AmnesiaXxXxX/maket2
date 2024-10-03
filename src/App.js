50ac78558b15c6801ee9c2b8eaf39f9dcff4bd56
amnesia <ai28092005@gmail.com>
2024-10-03 14:36:17 +0300

    add logos n pictures

--- src/App.js
+++ src/App.js
@@ -15,9 +15,21 @@ function App() {
           <button className="nav-button">News</button>
           <button className="nav-button">Contact</button>
         </nav>
-        <h2>Hello people! </h2>
+        <h2 id="title">Hello people! </h2>
         <h1>We’re creative<br/>digital studio.</h1>
-        <img></img>
+        <img className="logo" src="logo.png" width={125} height={125}></img>
+        <div className='logos'>
+
+        <img  src='logos.png' width={350} height={50}></img>
+        </div>
+        <h2 id="description1">Our Philosophy</h2>
+        <div id="description1__content">
+          <div id="pink__square" width={530} height={474}></div>
+          <p>Lorem Ipsum is simply dummy text of<br/> the printing and typesetting industry.</p>
+          <p>Lorem Ipsum has been the industry's<br/> standard dummy text ever since the<br/> 1500s, when an unknown printer took<br/> a galley of type and scrambled it to<br/> make a type specimen book.</p>
+          <button id="description1__content__entry-button">MEET THE TEAM</button>
+        </div>
+
       </div>
     </div>
   );
